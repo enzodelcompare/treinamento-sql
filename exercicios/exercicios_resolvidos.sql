105863534bb171b5d2687b357d0a9a4a9640baf3
Enzo Delcompare <78515983+enzodelcompare@users.noreply.github.com>
2024-10-23 08:23:02 -0300

    Update exercicios_resolvidos.sql

--- exercicios/exercicios_resolvidos.sql
+++ exercicios/exercicios_resolvidos.sql
@@ -1,1 +1,36 @@
-
+-- ###################################################################################################################
+-- ######################## LISTA DE EXERCÍCIOS [RESOLVIDOS] - TREINAMENTO DE SQL [COM MYSQL] ########################
+-- ###################################################################################################################
+--
+-- 001. Qual é a receita total gerada por cada filme na loja?
+-- 
+SELECT *
+  FROM ...
+-- 
+-- ----------
+-- 
+-- 002. Quais são os 10 filmes mais alugados e qual a sua receita total?
+-- 
+SELECT *
+  FROM ...
+-- 
+-- ----------
+-- 
+-- 003. Qual é o tempo médio de aluguel para os filmes?
+-- 
+SELECT *
+  FROM ...
+-- 
+-- ----------
+-- 
+-- 004. Como o comportamento dos clientes varia entre diferentes categorias de filmes?
+-- 
+SELECT *
+  FROM ...
+-- 
+-- ----------
+-- 
+-- 005. Qual é a distribuição de idades dos clientes que alugam filmes?
+-- 
+SELECT *
+  FROM ...
